521b21bec00c7fb96c8d98d12e5bef2c815bee2f
Deyvis Mariños <deyvisjaime17@gmail.com>
2023-11-01 00:02:58 -0500

    feat: update zustand state

--- src/zustand/useStore.ts
+++ src/zustand/useStore.ts
@@ -1,17 +1,33 @@
-import { ITokenData, IUser } from '@/interfaces/services'
+import {
+  ILinkCreateResponse,
+  IUser,
+  BankInfo,
+  IListTransactionsResponse
+} from '@/interfaces/services'
 import { create } from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
 
 interface IStoreState {
   user: IUser | null
+  bankLink: ILinkCreateResponse | null
+  selectedBank: BankInfo | null
+  listTransactions: IListTransactionsResponse['results'] | null
 }
 
 interface IStoreActions {
   setUser: (user: IUser | null) => void
+  setBankLink: (link: ILinkCreateResponse | null) => void
+  setSelectedBank: (bank: BankInfo | null) => void
+  setListTransactions: (
+    transactions: IListTransactionsResponse['results'] | null
+  ) => void
 }
 
 const initialState: IStoreState = {
-  user: null
+  user: null,
+  bankLink: null,
+  selectedBank: null,
+  listTransactions: null
 }
 
 export const useLocalStore = create<IStoreState & IStoreActions>()(
@@ -19,10 +35,24 @@ export const useLocalStore = create<IStoreState & IStoreActions>()(
     persist(
       (set) => ({
         user: initialState.user,
-
+        bankLink: initialState.bankLink,
+        selectedBank: initialState.selectedBank,
+        listTransactions: initialState.listTransactions,
         setUser: (v: IUser | null) =>
           set((state) => ({
             user: v
+          })),
+        setBankLink: (v: ILinkCreateResponse | null) =>
+          set((state) => ({
+            bankLink: v
+          })),
+        setSelectedBank: (v: BankInfo | null) =>
+          set((state) => ({
+            selectedBank: v
+          })),
+        setListTransactions: (v: IListTransactionsResponse['results'] | null) =>
+          set((state) => ({
+            listTransactions: v
           }))
       }),
       {
