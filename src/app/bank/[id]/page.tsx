841a8f0ed1ed9ebd896aff11f5e51be7ab67cdbd
Deyvis Mariños <deyvisjaime17@gmail.com>
2023-10-31 23:13:54 -0500

    feat: fetch server component

---
+++ src/app/bank/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { endpoints } from '@/services/endpoints'
+import { ITransactionsProps } from '@/interfaces/components/organisms'
+import { IListTransactionsResponse } from '@/interfaces/services'
+
+import { TransactionsContent } from '@/components/organisms/TransactionsContent'
+
+const Transactions = async ({ params: { id } }: ITransactionsProps) => {
+  const response = await fetch(
+    `${endpoints.bank.GET_LIST_TRANSACTIONS}?&page=1&page_size=50&link=${id}`,
+    {
+      headers: {
+        Authorization: `Basic ${process.env.NEXT_PUBLIC_SECRET_KEY}`
+      }
+    }
+  )
+
+  const listTransactions: IListTransactionsResponse = await response.json()
+
+  return <TransactionsContent transactions={listTransactions?.results} />
+}
+
+export default Transactions
