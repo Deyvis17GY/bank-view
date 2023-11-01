81d56f33b43b972bd49c0027d4f8d9bcbb8a91ba
Deyvis Mariños <deyvisjaime17@gmail.com>
2023-11-01 06:55:12 -0500

    feat: add try catch to fetch server component

--- src/app/bank/[id]/page.tsx
+++ src/app/bank/[id]/page.tsx
@@ -4,19 +4,34 @@ import { IListTransactionsResponse } from '@/interfaces/services'
 
 import { TransactionsContent } from '@/components/organisms/TransactionsContent'
 
-const Transactions = async ({ params: { id } }: ITransactionsProps) => {
-  const response = await fetch(
-    `${endpoints.bank.GET_LIST_TRANSACTIONS}?&page=1&page_size=20&link=${id}`,
-    {
-      cache: 'no-store',
-      headers: {
-        Authorization: `Basic ${process.env.NEXT_PUBLIC_SECRET_KEY}`
+const getFetchListTransactions = async (id: string) => {
+  try {
+    const response = await fetch(
+      `${endpoints.bank.GET_LIST_TRANSACTIONS}?&page=1&page_size=20&link=${id}`,
+      {
+        cache: 'no-store',
+        headers: {
+          Authorization: `Basic ${process.env.NEXT_PUBLIC_SECRET_KEY}`
+        }
       }
-    }
-  )
-  const listTransactions: IListTransactionsResponse = await response.json()
+    )
 
-  return <TransactionsContent transactions={listTransactions?.results} />
+    if (!response?.ok) {
+      return []
+    }
+
+    const listTransactions: IListTransactionsResponse = await response.json()
+    return listTransactions?.results
+  } catch (err) {
+    console.error(err)
+    return []
+  }
+}
+
+const Transactions = async ({ params: { id } }: ITransactionsProps) => {
+  const transactions = await getFetchListTransactions(id)
+
+  return <TransactionsContent transactions={transactions} />
 }
 
 export default Transactions
